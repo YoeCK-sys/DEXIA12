edba8324cd6ea3bb8e0dd4589ec5f27884ae4207
YoeCK-sys <114443942+YoeCK-sys@users.noreply.github.com>
2024-11-04 07:42:12 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-// pages/index.tsx
-import React from 'react';
-import MiComponente from '../components/component/component';
+import React from 'react'
+import MiComponente from '../components/component/component'
+import Head from 'next/head'
 
 const Home = () => {
   return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="yw-9QtjXju8t6o5ppc8TyqGnLrDr7L4UeBnxP63ii4g" />
+      </Head>
       <div>
-                  <MiComponente />
-                      </div>
-                        );
-                        };
+        <MiComponente />
+      </div>
+    </>
+  )
+}
 
-                        export default Home;
-
+export default Home
